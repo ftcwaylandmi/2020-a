b945a05a1407a01bcd77e646027607ba233907ee
ftcwaylandmi <ftc@waylandunion.org>
2020-10-27 19:16:45 -0400

    Fix A Repo

--- Robot.java
+++ Robot.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime; 
 import org.firstinspires.ftc.teamcode.RobotHardware;
 public class Robot {
     RobotHardware myself = new RobotHardware();
+    double InchesPerSecond=2.00;
     public void initHW(HardwareMap ahwMap){
         myself. init (ahwMap);
     }
@@ -47,4 +49,45 @@ public class Robot {
         myself.leftbackDrive.setPower(0);
         myself.rightbackDrive.setPower(s);
     }
+
+    public void DriveByInchesTimeSetPower( int inches, double power) {
+        double waitTime = 0.00;
+        if (inches > 0) {
+            power = power * -1;
+            waitTime = inches * InchesPerSecond;
+            myself.leftfrontDrive.setPower(power);
+            myself.leftbackDrive.setPower(power);
+            myself.rightfrontDrive.setPower(power);
+            myself.rightbackDrive.setPower(power);
+        } else {
+            waitTime = -inches * InchesPerSecond;
+            myself.leftfrontDrive.setPower(power);
+            myself.leftbackDrive.setPower(power);
+            myself.rightfrontDrive.setPower(power);
+            myself.rightbackDrive.setPower(power);
+        }
+        ElapsedTime timer =  new ElapsedTime();
+        timer.reset();
+        while (timer.milliseconds() < (waitTime* 100)) {
+
+        }
+        myself.leftfrontDrive.setPower(0);
+        myself.leftbackDrive.setPower(0);
+        myself.rightfrontDrive.setPower(0);
+        myself.rightbackDrive.setPower(0);
+
+    }
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
