e44a6c0a46fdbfe0d05d5fc1780bfac46eff9bd6
Wayland FTC Robotics <ftc@waylandunion.org>
2020-11-03 19:15:14 -0500

    Added TensorFlow

--- Robot.java
+++ Robot.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.RobotHardware;
 public class Robot {
     RobotHardware myself = new RobotHardware();
-    double InchesPerSecond=2.00;
+    double InchesPerSecond = 0.17;
     public void initHW(HardwareMap ahwMap){
         myself. init (ahwMap);
     }
@@ -38,15 +38,11 @@ public class Robot {
 
     public void RightDrive(double s) {
         myself.leftfrontDrive.setPower(s);
-        myself.rightfrontDrive.setPower(0);
         myself.leftbackDrive.setPower(s);
-        myself.rightbackDrive.setPower(0);
-    }
+       }
 
     public void LeftDrive(double s) {
-        myself.leftfrontDrive.setPower(0);
         myself.rightfrontDrive.setPower(s);
-        myself.leftbackDrive.setPower(0);
         myself.rightbackDrive.setPower(s);
     }
 
